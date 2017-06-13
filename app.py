58a80522cb5e77c7261cdbcf0467231f46e3bdcd
zybooks <zybooks@zybookss-MacBook-Air.local>
2017-06-13 11:59:38 -0700

    Addressed PR comments

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ with open('config_file.json') as config_file:
 
 conn = mdb.connect(host=config['host'], user=config['user'], passwd=config['password'], db=config['db'])
 
-
 cur = conn.cursor()
 
 @app.route('/')
@@ -23,12 +22,11 @@ def hello_world():
 def test():
     return 'Hello, test!'
 
-def getEvents(zybook_code, user_id):
+def get_events(zybook_code, user_id):
 	query = "SELECT event FROM user WHERE zybook_code=" + "'" + zybook_code + "'"
 	cur.execute(query)
 	rows = cur.fetchall()
 	user_rows = []
-	print(user_id)
 	for elem in rows:
 		if json.loads(elem[0])['event']['user_id'] == int(user_id):
 			user_rows.append(json.loads(elem[0])['event'])
@@ -36,17 +34,15 @@ def getEvents(zybook_code, user_id):
 	return user_rows
 
 @app.route('/zybook/<zybook_code>/events/<user_id>', methods = ['GET'])
-def getData(zybook_code, user_id):
-	user_rows = getEvents(zybook_code, user_id)
+def get_user_zybook_events(zybook_code, user_id):
+	user_rows = get_events(zybook_code, user_id)
 	return json.dumps({'success': True, 'events': user_rows})
 
 @app.route('/zybook/<zybook_code>/event', methods = ['POST'])
-def postData(zybook_code):
-	cur.execute("INSERT INTO user(zybook_code, event) VALUES(%s, %s)", (zybook_code, json.dumps({'event': {'chapter_number': request.json.get('chapterNum'), 'section_number': request.json.get('sectionNum'), 'user_id': request.json.get('user_id'), 'date': datetime.datetime.now().isoformat()}}))) #
-	#datetime.datetime.now().isoformat()
-	#event: chapter_num, section_num, user_id, date
+def record_zybook_event(zybook_code):
+	cur.execute("INSERT INTO user(zybook_code, event) VALUES(%s, %s)", (zybook_code, json.dumps({'event': {'chapter_number': request.json.get('chapterNum'), 'section_number': request.json.get('sectionNum'), 'user_id': request.json.get('user_id'), 'date': datetime.datetime.now().isoformat()}})))
 	conn.commit()
-	user_rows = getEvents(zybook_code, request.json.get('user_id'))
+	user_rows = get_events(zybook_code, request.json.get('user_id'))
 	return json.dumps({"success": True, 'events': user_rows})
 
 if __name__ == '__main__':
